de03eb96905ec1acf5effb879701c7652882aabc
Onkar Pande <onkardpande@gmail.com>
2018-08-15 10:23:30 +0530

    more values added

--- TreeCounting.java
+++ TreeCounting.java
@@ -58,14 +58,14 @@ class TreeCounting{
         if(root!=null)
         {
             inorderRec(root.left);
-            System.out.print("-> "+root.data+" ("+root.count+")");
+            System.out.print("-> "+root.data+"("+root.count+") ");
             inorderRec(root.right);
         }
     }
 
     public static void main(String[] args)
     {
-        int[] arr={5,6,3,9,3,6,2,1,5,4,3};
+        int[] arr={5,6,3,9,3,6,2,67,34,23,123,1,2,34,56,23,23,12,1,12,23,42,3424,52,34,234523,423,42,342,342,342,2345134,425,23,23,41,343,55,67,787,823,412,32,4456,56,78,67,88,7,54,63,563245,2,341,5,4,3};
 
         int n=arr.length;
 
